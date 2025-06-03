27755c5ac197fdcd0d79b4941465afec4faf7a8c
Darwin Herrera C <darwinsherrerac@gmail.com>
2025-06-02 21:11:20 -0500

    fix: Delete schema usecase name

--- src/domain/use_cases/schema/delete_schema_version.py
+++ src/domain/use_cases/schema/delete_schema_version.py
@@ -4,7 +4,7 @@ from bisslog import BasicUseCase, bisslog_db as db
 from bisslog.exceptions.domain_exception import NotFound
 
 
-class UpdateSchemaDefinition(BasicUseCase):
+class DeleteSchemaVersion(BasicUseCase):
     """
     Use case for updating the definition of a schema.
     """
